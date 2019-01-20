c0b3fed384f5ae958c03b011fbbf435816d1cc2b
Bill Gestrich <bill@foreflight.com>
2019-01-19 19:38:42 -0500

    Initial Commit

---
+++ JiraSwiftMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JiraSwiftMac
+//
+//  Created by Bill Gestrich on 1/19/19.
+//  Copyright © 2019 Bill Gestrich. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
